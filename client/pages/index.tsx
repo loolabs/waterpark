8a4e5e6604ed65d9b314ef764c96dfe8812356be
jeffzh4ng <14998054+jeffzh4ng@users.noreply.github.com>
2021-01-23 15:46:56 -0500

    Create skeleton of v0 (#5)
    Co-authored-by: Leslie Xin <leslie.xin@hotmail.com>

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ClubList from "./ClubList";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Waterpark</h1>
+      <ClubList />
+    </div>
+  );
+}
