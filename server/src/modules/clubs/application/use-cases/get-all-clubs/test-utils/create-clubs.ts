f98414d3cbc80fe276fb6c188f980da065e0c065
Kevin Tong <kevintong0821@gmail.com>
2021-02-10 19:45:15 -0500

    Changed _i to i

--- server/src/modules/clubs/application/use-cases/get-all-clubs/test-utils/create-clubs.ts
+++ server/src/modules/clubs/application/use-cases/get-all-clubs/test-utils/create-clubs.ts
@@ -3,10 +3,10 @@ import { Club } from '../../../../domain/entities/club'
 export const createMockClubs = (): Array<Club> => {
   
   const clubs: Array<Club> =[]
-  for (let _i = 1; _i <= 3; ++_i){
+  for (let i = 1; i <= 3; ++i){
     const clubResult = Club.create({
-        name: `Club Name ${_i}`,
-        description: `Club Description ${_i}`
+        name: `Club Name ${i}`,
+        description: `Club Description ${i}`
   })
 
   if (clubResult.isErr()) throw new Error('Something went wrong with Club creation')
