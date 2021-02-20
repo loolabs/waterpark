1cc8ab606cfcad1f5d9ca94d25e77af076ee1895
Kevin Tong <kevintong0821@gmail.com>
2021-02-20 16:44:58 -0500

    🗄 delete is_deleted column (#53)
    * delete is_deleted column
    
    * Remove Prettier(1)
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    * Remove Prettier(2)
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    * Remove Prettier(3)
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    * Remove Prettier(4)
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    * Remove Prettier(5)
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>

--- server/src/migrations/Migration20210123215506.ts
+++ server/src/migrations/Migration20210123215506.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20210123215506 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('create table "club" ("id" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "description" varchar(255) not null, "is_deleted" bool not null default false);');
+    this.addSql('create table "club" ("id" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "description" varchar(255) not null);');
     this.addSql('alter table "club" add constraint "club_pkey" primary key ("id");');
   }
 
