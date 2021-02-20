869b39bf64bfae74f47599fd8f8b164b7809b4a5
Kevin Tong <kevintong0821@gmail.com>
2021-02-20 16:35:14 -0500

    Remove Prettier(1)
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>

--- server/src/migrations/Migration20210123215506.ts
+++ server/src/migrations/Migration20210123215506.ts
@@ -1,4 +1,4 @@
-import { Migration } from '@mikro-orm/migrations'
+import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20210123215506 extends Migration {
   async up(): Promise<void> {
