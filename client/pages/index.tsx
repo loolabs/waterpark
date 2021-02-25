df2cf73ac0c97b0c5d37127cf7cf01d67a25cf0f
Justin Xu <xu.justin.j@gmail.com>
2021-02-24 21:49:45 -0500

    Add a UI element to display the PR number

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import Events from "./events";
+import React from 'react'
+import Events from './events'
 
 export default function Home() {
+  const isPR = process.env.VERCEL_GIT_IS_PULL_REQUEST === '1'
   return (
-    <Events/>
-  );
+    <div>
+      {isPR && <p>{process.env.VERCEL_GIT_PULL_REQUEST_NUMBER}</p>}
+      <Events />
+    </div>
+  )
 }
