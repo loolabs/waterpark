7cc6c2d13d67b762a5a1aaa9a3091327eee840ca
Justin Xu <xu.justin.j@gmail.com>
2021-03-13 19:54:34 -0500

    Repair Create User tests

--- server/src/modules/users/application/use-cases/create-user/__tests__/create-user-controller.test.unit.ts
+++ server/src/modules/users/application/use-cases/create-user/__tests__/create-user-controller.test.unit.ts
@@ -6,117 +6,109 @@ import { User } from '../../../../domain/entities/user'
 import { UserValueObjectErrors } from '../../../../domain/value-objects/errors'
 import { CreateUserDTO } from '../create-user-dto'
 import { CreateUserErrors } from '../create-user-errors'
+import { MockUserRepo } from '../../../../infra/repos/implementations/mock-user-repo'
+import { CreateUserController } from '../create-user-controller'
 import { CreateUserUseCase } from '../create-user-use-case'
-import { buildController } from '../test-utils/build-controller'
-import { createMockUser } from '../test-utils/create-user'
+import { UserMap } from '../../../../mappers/user-map'
+import { UserEmail } from '../../../../domain/value-objects/user-email'
+import { UserPassword } from '../../../../domain/value-objects/user-password'
 
 // TODO: how to show developer these mocks are necessary when building a controller? aka must be synced with buildController()
-jest.mock('../../../../infra/repos/implementations/mikro-user-repo')
 jest.mock('../create-user-use-case')
 
 describe('CreateUserController', () => {
-  let createUserDTO: CreateUserDTO
-  let mockUser: User
-
-  beforeAll(() => {
-    createUserDTO = {
-      email: 'john.doe@uwaterloo.ca',
-      password: 'secret',
-    }
-
-    mockUser = createMockUser()
-  })
+  const createUserDTO: CreateUserDTO = {
+    email: 'john.doe@uwaterloo.ca',
+    password: 'secret',
+  }
+  const createHttpMock = (body: any) => {
+    const req = httpMocks.createRequest({ body }) as DecodedExpressRequest
+    const res = httpMocks.createResponse()
+    return { req, res }
+  }
+  const createUser = ({ email, password }: CreateUserDTO): User => {
+    const userEmailResult = UserEmail.create(email)
+    const userPasswordResult = UserPassword.create({ value: password, hashed: false })
+    const results = [userEmailResult, userPasswordResult] as const
+    if (!Result.resultsAllOk(results)) throw Result.getFirstError(results)
+    const userResult = User.create({ email: results[0].value, password: results[1].value })
+    if (userResult.isErr()) throw userResult.error
+    return userResult.value
+  }
+  const setup = async (users: Array<User> = []) => {
+    const userRepo = new MockUserRepo(await Promise.all(users.map(UserMap.toPersistence)))
+    const createUserUseCase = new CreateUserUseCase(userRepo)
+    const createUserController = new CreateUserController(createUserUseCase)
+    return { userRepo, createUserUseCase, createUserController }
+  }
 
   test('When the CreateUserUseCase returns Ok, the CreateUserController returns 200 OK', async () => {
-    const mockRequest = httpMocks.createRequest({
-      body: createUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
-    jest.spyOn(CreateUserUseCase.prototype, 'execute').mockResolvedValue(Result.ok(mockUser))
-    const createUserController = buildController()
+    const { req, res } = createHttpMock(createUserDTO)
+    const user = createUser(createUserDTO)
+    jest.spyOn(CreateUserUseCase.prototype, 'execute').mockResolvedValue(Result.ok(user))
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
+    const result = await createUserController.execute(req, res)
 
     expect(result.statusCode).toBe(200)
   })
 
   test('When the CreateUserUseCase returns UserValueObjectErrors.InvalidEmail, CreateUserController returns 400 Bad Request', async () => {
-    const mockRequest = httpMocks.createRequest({
-      body: createUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
+    const { req, res } = createHttpMock(createUserDTO)
     jest
       .spyOn(CreateUserUseCase.prototype, 'execute')
       .mockResolvedValue(Result.err(new UserValueObjectErrors.InvalidEmail(createUserDTO.email)))
-    const createUserController = buildController()
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
+    const result = await createUserController.execute(req, res)
 
     expect(result.statusCode).toBe(400)
   })
 
   test('When the DTO is invalid, CreateUserController returns 400 Bad Request', async () => {
-    const invalidCreateUserDTO = {
-      email: 123,
-      password: 456,
-    }
-    const mockRequest = httpMocks.createRequest({
-      body: invalidCreateUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
-    jest.spyOn(CreateUserUseCase.prototype, 'execute').mockResolvedValue(Result.ok(mockUser))
-    const createUserController = buildController()
+    const invalidCreateUserDTO = Object.assign({}, createUserDTO) as any // shallow copy
+    invalidCreateUserDTO.email = 42
+    const { req, res } = createHttpMock(invalidCreateUserDTO)
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
-
+    const result = await createUserController.execute(req, res)
     expect(result.statusCode).toBe(400)
   })
 
   test('When the CreateUserUseCase returns UserValueObjectErrors.InvalidPassword, CreateUserController returns 400 Bad Request', async () => {
-    const mockRequest = httpMocks.createRequest({
-      body: createUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
+    const { req, res } = createHttpMock(createUserDTO)
     jest
       .spyOn(CreateUserUseCase.prototype, 'execute')
       .mockResolvedValue(
         Result.err(new UserValueObjectErrors.InvalidPassword(createUserDTO.password))
       )
-    const createUserController = buildController()
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
+    const result = await createUserController.execute(req, res)
     expect(result.statusCode).toBe(400)
   })
 
   test('When the CreateUserUseCase returns CreateUserErrors.EmailAlreadyExistsError, CreateUserController returns 409 Conflict', async () => {
-    const mockRequest = httpMocks.createRequest({
-      body: createUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
+    const { req, res } = createHttpMock(createUserDTO)
     jest
       .spyOn(CreateUserUseCase.prototype, 'execute')
       .mockResolvedValue(
         Result.err(new CreateUserErrors.EmailAlreadyExistsError(createUserDTO.email))
       )
-    const createUserController = buildController()
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
-
+    const result = await createUserController.execute(req, res)
     expect(result.statusCode).toBe(409)
   })
 
   test('When the CreateUserUseCase returns AppError.UnexpectedError, CreateUserController returns 500 Internal Server Error', async () => {
-    const mockRequest = httpMocks.createRequest({
-      body: createUserDTO,
-    }) as DecodedExpressRequest
-    const mockResponse = httpMocks.createResponse()
+    const { req, res } = createHttpMock(createUserDTO)
     jest
       .spyOn(CreateUserUseCase.prototype, 'execute')
       .mockResolvedValue(Result.err(new AppError.UnexpectedError('Unexpected error')))
-    const createUserController = buildController()
+    const { createUserController } = await setup()
 
-    const result = await createUserController.execute(mockRequest, mockResponse)
-
+    const result = await createUserController.execute(req, res)
     expect(result.statusCode).toBe(500)
   })
 })
--- server/src/modules/users/application/use-cases/create-user/__tests__/create-user-use-case.test.unit.ts
+++ server/src/modules/users/application/use-cases/create-user/__tests__/create-user-use-case.test.unit.ts
@@ -1,40 +1,44 @@
 import { Err } from '../../../../../../shared/core/result'
 import { User } from '../../../../domain/entities/user'
 import { UserValueObjectErrors } from '../../../../domain/value-objects/errors'
-import { MikroUserRepo } from '../../../../infra/repos/implementations/mikro-user-repo'
-import { UserRepo } from '../../../../infra/repos/user-repo'
+import { MockUserRepo } from '../../../../infra/repos/implementations/mock-user-repo'
+import { UserMap } from '../../../../mappers/user-map'
 import { CreateUserDTO } from '../create-user-dto'
+import { CreateUserErrors } from '../create-user-errors'
 import { CreateUserUseCase } from '../create-user-use-case'
-import { createMockUser } from '../test-utils/create-user'
 
-jest.mock('../../../../infra/repos/implementations/mikro-user-repo')
+jest.mock('../../../../infra/repos/implementations/mock-user-repo')
 
 describe('CreateUserUseCase', () => {
-  test('When executed with valid DTO, should save the user and return an Ok', async () => {
-    const createUserDTO: CreateUserDTO = {
+  let createUserDTO: CreateUserDTO
+
+  beforeEach(() => {
+    createUserDTO = {
       email: 'john.doe@uwaterloo.ca',
       password: 'secret',
     }
-    const userEntityRepo = undefined
-    const fakeMikroUserRepo = new MikroUserRepo(userEntityRepo)
-    const createUserUseCase = new CreateUserUseCase(fakeMikroUserRepo)
+  })
+
+  const setup = async (users: Array<User> = []) => {
+    const userRepo = new MockUserRepo(await Promise.all(users.map(UserMap.toPersistence)))
+    const createUserUseCase = new CreateUserUseCase(userRepo)
+    return { userRepo, createUserUseCase }
+  }
+
+  test('When executed with valid DTO, should save the user and return an Ok', async () => {
+    const { userRepo, createUserUseCase } = await setup()
 
     const createUserResult = await createUserUseCase.execute(createUserDTO)
 
-    expect(fakeMikroUserRepo.save).toBeCalled()
+    expect(userRepo.save).toBeCalled()
     expect(createUserResult.isOk()).toBe(true)
   })
 
   test('When executed with invalid email, should return UserValueObjectErrors.InvalidEmail', async () => {
-    const createUserDtoInvalidEmail: CreateUserDTO = {
-      email: 'john.doe@uft.ca',
-      password: 'secret',
-    }
-    const userEntityRepo = undefined
-    const fakeMikroUserRepo = new MikroUserRepo(userEntityRepo)
-    const createUserUseCase = new CreateUserUseCase(fakeMikroUserRepo)
+    createUserDTO.email = 'john.doe@mail.utoronto.ca'
+    const { createUserUseCase } = await setup()
 
-    const createUserResult = await createUserUseCase.execute(createUserDtoInvalidEmail)
+    const createUserResult = await createUserUseCase.execute(createUserDTO)
 
     expect(createUserResult.isErr()).toBe(true)
     const createUserErr = createUserResult as Err<User, UserValueObjectErrors.InvalidEmail>
@@ -42,15 +46,10 @@ describe('CreateUserUseCase', () => {
   })
 
   test('When executed with invalid password, should return UserValueObjectErrors.InvalidPassword', async () => {
-    const createUserDtoInvalidPassword: CreateUserDTO = {
-      email: 'john.doe@uwaterloo.ca',
-      password: '2shrt',
-    }
-    const userEntityRepo = undefined
-    const fakeMikroUserRepo = new MikroUserRepo(userEntityRepo)
-    const createUserUseCase = new CreateUserUseCase(fakeMikroUserRepo)
+    createUserDTO.password = '2shrt'
+    const { createUserUseCase } = await setup()
 
-    const createUserResult = await createUserUseCase.execute(createUserDtoInvalidPassword)
+    const createUserResult = await createUserUseCase.execute(createUserDTO)
 
     expect(createUserResult.isErr()).toBe(true)
     const createUserErr = createUserResult as Err<User, UserValueObjectErrors.InvalidPassword>
@@ -58,22 +57,13 @@ describe('CreateUserUseCase', () => {
   })
 
   test('When executed with email that already exists, should return CreateUserErrors.EmailAlreadyExistsError', async () => {
-    const createUserDTO: CreateUserDTO = {
-      email: 'john.doe@uwaterloo.ca',
-      password: '2shrt',
-    }
-    const user = createMockUser()
-    const fakeUserRepo: UserRepo = {
-      exists: async () => true,
-      getUserByUserId: async () => user,
-      save: async () => {},
-    }
-    const createUserUseCase = new CreateUserUseCase(fakeUserRepo)
+    jest.spyOn(MockUserRepo.prototype, 'exists').mockResolvedValue(true)
+    const { createUserUseCase } = await setup()
 
     const createUserResult = await createUserUseCase.execute(createUserDTO)
 
     expect(createUserResult.isErr()).toBe(true)
-    const createUserErr = createUserResult as Err<User, UserValueObjectErrors.InvalidPassword>
-    expect(createUserErr.error instanceof UserValueObjectErrors.InvalidPassword).toBe(true)
+    const createUserErr = createUserResult as Err<User, CreateUserErrors.EmailAlreadyExistsError>
+    expect(createUserErr.error instanceof CreateUserErrors.EmailAlreadyExistsError).toBe(true)
   })
 })
