f902d975a4efa8173ee8e28985cb372c060547c9
lesliexin <leslie.xin@hotmail.com>
2021-03-20 16:37:39 -0400

    Fixing mobile logo showing on initial load

--- client/components/hooks/UseViewport.tsx
+++ client/components/hooks/UseViewport.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 export const useViewport = () => {
-    const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth)
 
-    useEffect(() => {
-        const handleWindowResize = () => setWidth(window.innerWidth);
-        window.addEventListener("resize", handleWindowResize);
-        return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleWindowResize)
+    return () => window.removeEventListener('resize', handleWindowResize)
+  }, [])
 
-    return { width };
+  return { width }
 }
