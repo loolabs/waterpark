7331a34437178457d5cd33ee477461ff4ed9edd6
Leslie Xin <leslie.xin@hotmail.com>
2021-03-20 16:13:22 -0400

    Creating Nav Bar (#75)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AppProvider } from "../context";
+import { AppProvider } from "../context"
+import { NavBar } from "../components/NavBar"
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
+      <NavBar/>
       <Component {...pageProps} />
     </AppProvider>
   )
--- client/styles/typography.ts
+++ client/styles/typography.ts
@@ -1,83 +1,63 @@
-import styled from "styled-components";
-
-import { colours } from "./colours";
-import { device } from "./device";
+import styled from 'styled-components'
+import { colours } from './colours'
+import { device } from './device'
 
 // font size
-const text48 = "48px";
-const text36 = "36px";
-const text32 = "32px";
-const text30 = "30px";
-const text24 = "24px";
-const text20 = "20px";
-const text18 = "18px";
-const text16 = "16px";
-const text14 = "14px";
-const text12 = "12px";
-const text10 = "10px";
+const text48 = '48px'
+const text36 = '36px'
+const text32 = '32px'
+const text30 = '30px'
+const text24 = '24px'
+const text20 = '20px'
+const text18 = '18px'
+const text16 = '16px'
+const text14 = '14px'
+const text12 = '12px'
+const text10 = '10px'
 
 // font weight
-const weight900 = "900";
-const weight700 = "700";
-const weight600 = "600";
-const weight400 = "400";
+const weight900 = '900'
+const weight700 = '700'
+const weight600 = '600'
+const weight400 = '400'
 
 // font style
-const fontBrandonGrotesque = "Brandon Grotesque";
-const fontInter = "Inter";
+const fontInter = 'Inter'
 
 export const desktopFontSize = Object.freeze({
-    h1: text48,
-    h2: text32,
-    h3: text24,
-    subtitle1: text20,
-    subtitle2: text18,
-    body1: text16,
-    body2: text12,
-});
+  h1: text48,
+  h2: text32,
+  h3: text24,
+  subtitle1: text20,
+  subtitle2: text18,
+  body1: text16,
+  body2: text12,
+})
 
 export const mobileFontSize = Object.freeze({
-    h1: text30,
-    h2: text24,
-    h3: text20,
-    subtitle1: text18,
-    subtitle2: text16,
-    body1: text14,
-    body2: text12,
-    body3: text10,
-});
+  h1: text30,
+  h2: text24,
+  h3: text20,
+  subtitle1: text18,
+  subtitle2: text16,
+  body1: text14,
+  body2: text12,
+  body3: text10,
+})
 
 export const fontWeight = Object.freeze({
-    bold: weight700,
-    semiBold: weight600,
-    regular: weight400,
-});
-
-export const fontFamily = Object.freeze({
-    primary: fontInter,
-    secondary: fontBrandonGrotesque,
-});
-
-export const Waterpark = styled.span`
-    font-family: ${fontFamily.secondary};
-    font-style: normal;
-    font-weight: ${weight900};
-    font-size: ${text36};
-    line-height: 120%;
-    color: ${colours.black};
-    @media ${device.laptop} {
-        font-size: ${text30};
-    }
-`;
+  bold: weight700,
+  semiBold: weight600,
+  regular: weight400,
+})
 
 export const PageTitle = styled.span`
-    font-family: ${fontFamily.primary};
-    font-style: normal;
-    font-weight: ${fontWeight.semiBold};
-    font-size: ${desktopFontSize.h1};
-    line-height: 120%;
-    color: ${colours.black};
-    @media ${device.laptop} {
-        font-size: ${mobileFontSize.h1};
-    }
-`;
+  font-style: normal;
+  font-weight: ${fontWeight.semiBold};
+  font-size: ${desktopFontSize.h1};
+  line-height: 120%;
+  color: ${colours.black};
+  @media ${device.laptop} {
+    font-size: ${mobileFontSize.h1};
+  }
+`
