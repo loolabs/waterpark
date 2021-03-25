b95546b916a586183525ab6a62347c23c3658d27
Justin Xu <xu.justin.j@gmail.com>
2021-03-24 22:46:47 -0400

    Code check-in for club cards

--- client/components/clubs/ClubCard.tsx
+++ client/components/clubs/ClubCard.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { Club } from '../../context'
+import styled from 'styled-components'
 
 interface ClubCardProps {
   club: Club
 }
 
+const ClubCardContainer = styled.div`
+  border-radius: 6px;
+  overflow: hidden;
+  max-width: 450px;
+`
+
+const ClubCardHeader = styled.div<{ backgroundImage: string }>`
+  width: 100%;
+  height: 0;
+  padding-top: calc(100% / (64 / 27));
+  background-image: url(${({ backgroundImage }) => backgroundImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
+const ClubCardContent = styled.div`
+  margin: 3%;
+  height: 200px;
+`
+
 export const ClubCard = ({ club }: ClubCardProps) => {
-  const { id, name, description } = club
+  const { id, name, description, iconURL } = club
   const router = useRouter()
 
   const handleClick = () => {
@@ -15,9 +37,12 @@ export const ClubCard = ({ club }: ClubCardProps) => {
   }
 
   return (
-    <div onClick={handleClick}>
-      <h1>{name}</h1>
-      <p>{description}</p>
-    </div>
+    <ClubCardContainer onClick={handleClick}>
+      <ClubCardHeader backgroundImage={iconURL}></ClubCardHeader>
+      <ClubCardContent>
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </ClubCardContent>
+    </ClubCardContainer>
   )
 }
