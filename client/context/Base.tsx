c916f9c9fdf071887d47ec57dbb8392fb51bc6e5
ShreyasPrasad <missonpkmn@gmail.com>
2021-03-27 15:03:26 -0400

    changed img field on Event to backgroundImageURL

--- client/context/Base.tsx
+++ client/context/Base.tsx
@@ -35,7 +35,7 @@ export type Event = {
   name: string,
   club: string,
   description: string,
-  img: string,
+  backgroundImageURL: string,
   startDate: string,
   endDate: string,
   tags: Array<string>
@@ -89,7 +89,7 @@ export const EVENTS: Array<Event> = [
     name: 'Tech+ Mock Interview',
     club: 'Tech + UW',
     description: 'dancing in the dark',
-    img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    backgroundImageURL: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
     startDate: "2021-24-03 18:00",
     endDate: "2021-24-03 20:00",
     tags: ["Tech", "Community"]
@@ -99,7 +99,7 @@ export const EVENTS: Array<Event> = [
     name: 'ARBUS Society Movie Night',
     club: 'ARBUS',
     description: 'chef curry with the shot',
-    img: "https://images.unsplash.com/photo-1616169201999-0d80789e41c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    backgroundImageURL: "https://images.unsplash.com/photo-1616169201999-0d80789e41c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
     startDate: "2021-24-04 8:00",
     endDate: "2021-24-04 10:00",
     tags: ["Arts"]
@@ -109,7 +109,7 @@ export const EVENTS: Array<Event> = [
     name: 'UWACC Open Auditions',
     club: 'UWACC',
     description: '👩‍🔬',
-    img: "https://images.unsplash.com/photo-1616256074022-3a20e0e8bf1b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+    backgroundImageURL: "https://images.unsplash.com/photo-1616256074022-3a20e0e8bf1b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
     startDate: "2021-24-05 10:00",
     endDate: "2021-24-05 12:00",
     tags: ["Science", "Health"]
