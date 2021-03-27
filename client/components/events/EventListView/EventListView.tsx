c854f2d571c1d3d20cd256ca5f89ff309e0838ab
ShreyasPrasad <missonpkmn@gmail.com>
2021-03-27 14:36:45 -0400

    added date label function to event list view

--- client/components/events/EventListView/EventListView.tsx
+++ client/components/events/EventListView/EventListView.tsx
@@ -1,5 +1,6 @@
 import { EventListCard } from './EventListCard'
 import { Id, Event } from '../../../context'
+import moment from 'moment'
 import styled from 'styled-components'
 
 interface EventViewProps {
@@ -7,15 +8,35 @@ interface EventViewProps {
 }
 
 const ListView = styled.div<any>`
-  width: 1050px;
+  width: 100%;
+`
+
+const ListViewRow = styled.div<any>`
+  display: flex;
+`
+
+const ListViewDate = styled.div<any>`
+  display: flex;
 `
 
 export const EventListView = ({ filteredEvents }: EventViewProps) => {
+  
+  const getDateLabelIfNecessary = (index: number, allEvents: Event[]) => {
+    if(index == 0 || moment(allEvents[index-1].startDate) != moment(allEvents[index].startDate)){
+      return moment(allEvents[index].startDate).format("MMM Do YY");
+    }
+    return null;
+  }
 
   return (
-    <ListView>
-      {filteredEvents.map((event) => (
-        <EventListCard key={event.id} event={event} />
+    <ListView> 
+      {filteredEvents.map((event, index, allEvents) => (
+        <ListViewRow>
+          <ListViewDate>
+            {getDateLabelIfNecessary(index, allEvents)}
+          </ListViewDate>
+          <EventListCard key={event.id} event={event} />
+        </ListViewRow>
       ))}
     </ListView>
   )
