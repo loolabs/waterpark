8fadff6afa392f1595043a7c5ce2c085b5a100ed
Jeff Zhang <zhang.jeffreyd@gmail.com>
2021-04-26 13:20:59 -0400

    Remove a lot of stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,36 @@
 import express from 'express'
-import { RequestContext } from '@mikro-orm/core'
-import { MikroORM } from '@mikro-orm/core/MikroORM'
+// import { RequestContext } from '@mikro-orm/core'
+// import { MikroORM } from '@mikro-orm/core/MikroORM'
 import { v1Router } from './shared/infra/http/routes'
-import mikroORMConfig from './mikro-orm.config'
-import { DB } from './shared/infra/db'
-import { UserEntity } from './shared/infra/db/entities/user.entity'
-import { ClubEntity } from './shared/infra/db/entities/club.entity'
-import { EventEntity } from './shared/infra/db/entities/event.entity'
-import { TagEntity } from './shared/infra/db/entities/tags/tag.entity'
+// import mikroORMConfig from './mikro-orm.config'
+// import { DB } from './shared/infra/db'
+// import { UserEntity } from './shared/infra/db/entities/user.entity'
+// import { ClubEntity } from './shared/infra/db/entities/club.entity'
+// import { EventEntity } from './shared/infra/db/entities/event.entity'
+// import { TagEntity } from './shared/infra/db/entities/tags/tag.entity'
 
 const app = express()
 
 const port = process.env.PORT || 3001
 
-const initializeORM = async (DBObject: typeof DB) => {
-  DBObject.orm = await MikroORM.init(mikroORMConfig)
-  DBObject.em = DBObject.orm.em
-  DBObject.usersEntityRepo = DBObject.orm.em.getRepository(UserEntity)
-  DBObject.clubsEntityRepo = DBObject.orm.em.getRepository(ClubEntity)
-  DBObject.eventsEntityRepo = DBObject.orm.em.getRepository(EventEntity)
-  DBObject.tagsEntityRepo = DBObject.orm.em.getRepository(TagEntity)
-  const migrator = DBObject.orm.getMigrator()
-  await migrator.up()
-}
+// const initializeORM = async (DBObject: typeof DB) => {
+//   DBObject.orm = await MikroORM.init(mikroORMConfig)
+//   DBObject.em = DBObject.orm.em
+//   DBObject.usersEntityRepo = DBObject.orm.em.getRepository(UserEntity)
+//   DBObject.clubsEntityRepo = DBObject.orm.em.getRepository(ClubEntity)
+//   DBObject.eventsEntityRepo = DBObject.orm.em.getRepository(EventEntity)
+//   DBObject.tagsEntityRepo = DBObject.orm.em.getRepository(TagEntity)
+//   const migrator = DBObject.orm.getMigrator()
+//   await migrator.up()
+// }
 
 const bootstrap = async () => {
-  app.use(express.json())
+  // app.use(express.json())
 
-  app.use((_req, _res, next) => RequestContext.create(DB.orm.em, next))
+  // app.use((_req, _res, next) => RequestContext.create(DB.orm.em, next))
   app.use('/api/v1', v1Router)
 
-  app.use((_req, res) => res.status(404).json({ message: 'No route found' }))
+  // app.use((_req, res) => res.status(404).json({ message: 'No route found' }))
   app.listen(port, () => {
     console.log(`Waterpark REST API server running on http://localhost:${port}/api/v1 🦆`)
   })
