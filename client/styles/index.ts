8638733fbcd7d193ccd4b26c5a5fbfc4ccd45f51
Justin Xu <xu.justin.j@gmail.com>
2021-05-01 03:54:28 -0400

    💻 Implement Club List (#77)
    * Code check-in for club cards
    
    * Rebase and continue work
    
    * Layout club cards and tags
    
    * Colorize club tags and add searchbar
    
    * Finish club list for now
    
    * Code cleanup
    
    * Remove unused index in map
    
    * Roll back package-lock changes
    
    * Address feedback from design review
    
    * Rename Search to SearchInput
    
    * Make content slightly more appropriate
    
    * Stick to ellipses for text overflow
    
    * General cleanup

--- client/styles/index.ts
+++ client/styles/index.ts
@@ -1,3 +1,3 @@
 export { colours } from './colours'
 export { device } from './device'
-export { desktopFontSize, mobileFontSize, fontWeight, PageTitle } from './typography'
+export { desktopFontSize, mobileFontSize, fontInter, fontWeight, PageTitle } from './typography'
