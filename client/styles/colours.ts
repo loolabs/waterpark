f14524ef6149eb4ae41216c3da224e05f86bcf5b
Shreyas Krishna Prasad <missonpkmn@gmail.com>
2021-05-10 10:40:49 -0400

    59/eventscalendarview (#81)
    * setup basic listview and calendarview toggle
    
    * created new view components for events
    
    * completed markup and styling for eventcalendarview
    
    * broke up calendar view code into 2 files, and finished markup for event listview
    
    * added date label function to event list view
    
    * added date format
    
    * added styling to event listview
    
    * completed styled events listview and calendar view
    
    * addressed PR comments
    
    * addressed justin's PR comments
    
    * implemented feedback from design and justin
    
    * got rid of shorthand flex property in listcard
    
    * addressed justin's comments and refactored filtered events into ordered map
    
    * spacing
    
    * accidentally omitted these changes in previous commit, syncing with main
    
    * relocated eventviewtoggle and removed dateOrder array from eventview

--- client/styles/colours.ts
+++ client/styles/colours.ts
@@ -20,6 +20,22 @@ const greyLight2 = "#A3A3A3";
 const greyDark1 = "#8C8C8C";
 const greyDark2 = "#666666";
 
+const tagColours = {
+    'Community': '#FCF4B1',
+    'Tech': '#BCFEF2',
+    'Creative': '#FCB8C6', 
+    'Active': '#F9B5B5',
+    'Gaming': '#F3FDB6',
+    'Career': '#B8FDC7',
+    'Volunteer': '#FEE2B8',
+    'Science': '#B6D5FC',
+    'Environment': '#D1E77E',
+    'Health': '#97DFEF',
+    'Arts': '#FDD5A8',
+    'Math': '#FDC1EE',
+    'Engineering': '#D0B4EC'
+}
+
 export const colours = Object.freeze({
     primary1: purple,
     primary2: blueDark,
@@ -34,5 +50,6 @@ export const colours = Object.freeze({
     neutralLight1: greyLight1,
     neutralLight2: greyLight2,
     neutralDark1: greyDark1,
-    neutralDark2: greyDark2
+    neutralDark2: greyDark2,
+    tagColours: tagColours
 });
