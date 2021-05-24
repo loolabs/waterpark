967fb36b64aa61934df9efea58840de267301162
richmondvan <evan.ty.zheng@gmail.com>
2021-05-24 13:49:38 -0400

    Run prettier, remove redundancies

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { useAppContext } from '../../context'
 import { LoginPage } from '../../components/login/LoginPage'
 
 export default function Login() {
-
   return <LoginPage />
 }
