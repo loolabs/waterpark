2195d69669d6c4cf25af1848a42174f7e63d4913
Anson Yuen <74753389+a22yuen@users.noreply.github.com>
2021-07-18 02:41:41 -0400

    💻 Add Reviews to Resources (#169)
    * update review parameters and mock data
    
    * display review entries under reviews section
    
    * Update placeholder data
    
    * Add react-stars library
    
    * Export additional breakpoints
    
    * Style review cards
    
    * Tweak styling of ratings
    
    * Add placeholder for author/timestamp info
    
    * Run prettier
    
    * Fix review rating layout on mobile
    
    * Run prettier
    
    * Add timestamps and faculties
    
    * fix faculty typo engineering -> Engineering
    
    * fix faculty error in reviews
    
    * format styled components, attaching styles to components, remove wrappers
    
    * Rename "Math" -> "Mathematics"
    
    * Fix avatar margins on large screens
    
    * Pass in label and score for ratings better
    
    * Use date-fns library to format review timestamp
    
    * Sort reviews by timestamp by default
    
    * Add reviewer status option
    
    * Fix merge errors
    
    * Update client/components/resource-details/ResourceInfo.tsx
    
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>
    
    Co-authored-by: richmondvan <evan.ty.zheng@gmail.com>
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>

--- client/components/resource-details/ResourceInfo.tsx
+++ client/components/resource-details/ResourceInfo.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
-import { colours, fontWeight, PageTitle, width, largerThan } from '../../styles'
+import { colours, fontWeight, PageTitle, largerThan, smallerThan, width } from '../../styles'
 import { Resource } from '../../utils'
 import { Review } from '../../utils/types'
 import { TagBubble } from '../common/TagBubble'
+import ReactStars from 'react-stars'
 import ImageGallery from 'react-image-gallery'
+import { formatRelative } from 'date-fns'
 
 const BackArrow = styled.img`
   margin-top: 64px;
@@ -26,15 +28,15 @@ const Name = styled(PageTitle)`
   margin-top: 32px;
 `
 
-const Links = styled.div`
-  display: flex;
-  margin-top: 16px;
-`
+// const Links = styled.div`
+//   display: flex;
+//   margin-top: 16px;
+// `
 
-const SocialLink = styled.a`
-  margin-right: 32px;
-  text-decoration: underline;
-`
+// const SocialLink = styled.a`
+//   margin-right: 32px;
+//   text-decoration: underline;
+// `
 
 const Metadata = styled.div`
   align-items: center;
@@ -49,15 +51,22 @@ const MemberCount = styled.p`
   margin-bottom: 0;
 `
 
-const CategoriesWrapper = styled.div`
-  display: flex;
-  margin-right: -4px;
-`
+// const CategoriesWrapper = styled.div`
+// display: flex;
+// margin-right: -4px;
+// `
+// const Categories = ({ tags }: { tags: Array<string> }) => (
+//   <CategoriesWrapper>
+//     {tags.map((t, i) => (
+//       <TagBubble key={`category-tag-${i}`} colour={colours.tagColours[t]}>
+//         {t}
+//       </TagBubble>
+//     ))}
+//   </CategoriesWrapper>
+// )
 
 const Description = styled.p`
   line-height: 1.3;
-  margin-top: 24px;
-  margin-bottom: 24px;
 `
 
 interface ResourceInfoProps {
@@ -66,8 +75,7 @@ interface ResourceInfoProps {
 
 export const ResourceInfo = ({ resource }: ResourceInfoProps) => {
   const router = useRouter()
-
-  const { name, description, links, galleryImages } = resource
+  const { name, description, links, galleryImages, reviews } = resource
   // const potentialLinks = [links.facebook, links.twitter, links.instagram, links.website]
   // const linksThatExist = potentialLinks.filter((link) => link !== undefined)
 
@@ -95,41 +103,21 @@ export const ResourceInfo = ({ resource }: ResourceInfoProps) => {
 
       <Description>{description}</Description>
       <Gallery links={galleryImages} />
-      <Reviews reviews={[]} />
+      <Reviews reviews={reviews} />
     </div>
   )
 }
 
-const Categories = ({ tags }: { tags: Array<string> }) => (
-  <CategoriesWrapper>
-    {tags.map((t, i) => (
-      <TagBubble key={`category-tag-${i}`} colour={colours.tagColours[t]}>
-        {t}
-      </TagBubble>
-    ))}
-  </CategoriesWrapper>
-)
-
-const GalleryWrapper = styled.div`
-  width: 100%;
-
-  @media ${largerThan(width.laptop)} {
-    width: 60%;
-  }
-`
-
-const Gallery = ({ links }: { links: Array<string> }) => {
+const Gallery = styled(({ className, links }: { links: Array<string>; className?: string }) => {
   const images = links.map((link) => {
     return {
       original: link,
       thumbnail: link,
     }
   })
-
   return (
-    <GalleryWrapper>
+    <div className={className}>
       <h1>Gallery</h1>
-
       <ImageGallery
         items={images}
         showPlayButton={false}
@@ -161,41 +149,158 @@ const Gallery = ({ links }: { links: Array<string> }) => {
           )
         }}
       />
-    </GalleryWrapper>
+    </div>
   )
-}
+})`
+  width: 100%;
+  @media ${largerThan(width.laptop)} {
+    width: 60%;
+  }
+`
 
 interface ReviewsProps {
   reviews: Array<Review>
 }
 
 const Reviews = ({ reviews }: ReviewsProps) => {
+  reviews.sort((a, b) => (b.timestamp.getTime() - a.timestamp.getTime()));
   return (
     <div>
       <h1>Reviews</h1>
-      {reviews.map((review) => {
-        return <ReviewEntry review={review} />
+      {reviews.map((review, index) => {
+        return <ReviewCard key={index} review={review} />
       })}
     </div>
   )
 }
 
 const Avatar = styled.img`
-  border-radius: 4px;
+  border-radius: 50%;
   display: block;
-  margin-top: 40px;
-  height: 100px;
-  width: 100px;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-top: 24px;
+  height: 80px;
+  width: 80px;
+
+  @media ${largerThan(width.laptop)} {
+    margin-bottom: 24px;
+  }
 `
 
-const Rating = styled.p``
+const RatingList = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  margin: 24px;
+  margin-left: auto;
+  align-self: flex-start;
+  @media ${smallerThan(width.mobileS)} {
+    margin-left: 24px;
+  }
+`
 
-const ReviewEntry = ({ review }: { review: Review }) => {
+const ReviewTopRow = styled.div`
+  display: flex;
+  @media ${smallerThan(width.laptop)} {
+    flex-wrap: wrap;
+  }
+`
+
+const ShowOnLaptop = styled.div`
+  @media ${smallerThan(width.laptop)} {
+    display: none;
+  }
+`
+
+const HideOnLaptop = styled.div`
+  @media ${largerThan(width.laptop)} {
+    display: none;
+  }
+`
+
+const AuthorInfo = styled.div`
+  font-style: italic;
+`
+
+const Comment = styled(({ className, review }: { className?: string; review: Review }) => {
   return (
-    <>
-      <Avatar />
-      <Description />
-      <Rating />
-    </>
+    <div className={className}>
+      <Description>{review.comment}</Description>
+      <AuthorInfo>
+        — {review.faculty} {review.status}, {formatRelative(review.timestamp, Date.now())}
+      </AuthorInfo>
+    </div>
   )
-}
+})`
+  margin: 24px;
+`
+
+const ReviewCard = styled(({ review, className }: { review: Review; className?: string }) => {
+  return (
+    <div className={className}>
+      <ReviewTopRow>
+        <Avatar src={review.avatarImage}></Avatar>
+        <ShowOnLaptop>
+          <Comment review={review} />
+        </ShowOnLaptop>
+        <RatingList>
+          {Object.entries(review.ratings).map(([label, score]) => {
+            return <Rating score={score} key={label} label={label}></Rating>
+          })}
+        </RatingList>
+      </ReviewTopRow>
+      <HideOnLaptop>
+        <Comment review={review} />
+      </HideOnLaptop>
+    </div>
+  )
+})`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  border-radius: 8px;
+  overflow: hidden;
+  width: 100%;
+
+  // A subtle shadow around the card
+  box-shadow: 0px 4px 3px 2px ${colours.neutralLight1};
+`
+
+const RatingLabel = styled.p`
+  margin-bottom: 5px;
+  margin-top: 5px;
+  margin-right: 5px;
+  padding-left: auto;
+  flex: 1;
+  flex-grow: 1;
+  font-weight: 600;
+  width: auto;
+  color: ${colours.neutralDark2};
+`
+
+const Rating = styled(({ className, score, label }: { className?: string; score: number; label: string}) => {
+  return (
+    <div className={className}>
+      <RatingLabel>{label.toUpperCase()}:</RatingLabel>
+      <ReactStars
+        count={5}
+        char={'●'}
+        value={score / 20}
+        size={24}
+        color1={'#DDDDDD'}
+        color2={colours.primary1}
+        edit={false}
+      />
+    </div>
+  )
+})`
+  @media ${smallerThan(width.laptop)} {
+    display: flex;
+    margin-right: 0;
+    flex-wrap: wrap;
+  }
+
+  @media ${smallerThan(width.tablet)} {
+    width: 120px;
+  }
+`
--- client/utils/types.ts
+++ client/utils/types.ts
@@ -14,22 +14,75 @@ export interface Resource {
   }
   galleryImages: Array<URL>
   overallRating: number
-  review: Array<{
-    comment: string
-    rating: number
-  }>
+  reviews: Array<Review>
 }
 
-export interface House extends Resource {}
+export interface House extends Resource {
+  resourceSlug: 'housing'
+  reviews: Array<HousingReview>
+}
 
-export interface StudySpot extends Resource {}
+export interface StudySpot extends Resource {
+  resourceSlug: 'study-spots'
+  reviews: Array<StudySpotReview>
+}
 
-export interface Washroom extends Resource {}
+export interface Washroom extends Resource {
+  resourceSlug: 'washrooms'
+  reviews: Array<WashroomReview>
+}
 
 export type Id = number
+
+export enum Faculty {
+  Mathematics = 'Mathematics',
+  Engineering = 'Engineering',
+  Arts = 'Arts',
+  Health = 'Health',
+  Science = 'Science',
+  Environment = 'Environment',
+  NonWaterloo = 'Non-Waterloo'
+}
+
+export enum Status {
+  U1 = 'First-Year Student',
+  U2 = 'Second-Year Student',
+  U3 = 'Third-Year Student',
+  U4 = 'Fourth-Year Student',
+  U5 = 'Fifth-Year Student',
+  U6Plus = 'Sixth-Year+ Student',
+  Masters = 'Master\'s Student',
+  PhD = 'PhD Student',
+  Faculty = 'Faculty Member',
+  Other = 'Reviewer'
+}
 
 export interface Review {
   avatarImage: string
   comment: string
-  rating: number
+  timestamp: Date
+  faculty: Faculty
+  status: Status
+  ratings: { [name: string]: number }
+}
+
+export interface HousingReview extends Review {
+  ratings: {
+    cleaniness: number
+    price: number
+    management: number
+  }
+}
+
+export interface StudySpotReview extends Review {
+  ratings: {
+    cleaniness: number
+    noise: number
+  }
+}
+
+export interface WashroomReview extends Review {
+  ratings: {
+    cleaniness: number
+  }
 }
