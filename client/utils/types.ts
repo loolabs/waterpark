7f4fbb61e22a6dad98f5c98b40ffcb3537532ede
Evan Zheng <evan.ty.zheng@gmail.com>
2021-07-24 20:38:27 -0400

    Revert changes to types.ts

--- client/utils/types.ts
+++ client/utils/types.ts
@@ -41,7 +41,7 @@ export enum Faculty {
   Health = 'Health',
   Science = 'Science',
   Environment = 'Environment',
-  NonWaterloo = 'Non-Waterloo',
+  NonWaterloo = 'Non-Waterloo'
 }
 
 export enum Status {
@@ -54,7 +54,7 @@ export enum Status {
   Masters = "Master's Student",
   PhD = 'PhD Student',
   Faculty = 'Faculty Member',
-  Other = 'Reviewer',
+  Other = 'Reviewer'
 }
 
 export interface Review {
