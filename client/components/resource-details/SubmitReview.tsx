5e823ce856ad8fc3d7039b20f851e0f721525879
Evan Zheng <evan.ty.zheng@gmail.com>
2021-07-31 14:09:25 -0400

    Add styling on close button

--- client/components/resource-details/SubmitReview.tsx
+++ client/components/resource-details/SubmitReview.tsx
@@ -233,6 +233,14 @@ const DropdownLabel = styled.label`
   margin-right: 10px;
 `
 
+const CloseButton = styled.svg`
+  fill: rgb(100, 100, 100);
+  cursor: pointer;
+  &:hover {
+    fill: rgb(0, 0, 0);
+  }
+`
+
 const ModalContent = ({
   onRequestClose,
   resourceSlug,
@@ -281,7 +289,7 @@ const ModalContent = ({
     <>
       <ModalHeaderRow>
         <ModalTitle>Add a Review</ModalTitle>
-        <svg
+        <CloseButton
           onClick={onRequestClose}
           xmlns="http://www.w3.org/2000/svg"
           height="24px"
@@ -291,7 +299,7 @@ const ModalContent = ({
         >
           <path d="M0 0h24v24H0V0z" fill="none" />
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-        </svg>
+        </CloseButton>
       </ModalHeaderRow>
       <form>
         {Object.keys(ratings).map((name) => {
