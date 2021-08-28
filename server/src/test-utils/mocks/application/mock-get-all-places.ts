8620a79a38e4d5133d4b83f8fc39932f7e19d804
Justin Xu <xu.justin.j@gmail.com>
2021-08-28 15:57:18 -0400

    Fix argument naming in mockGetAllPlaces

--- server/src/test-utils/mocks/application/mock-get-all-places.ts
+++ server/src/test-utils/mocks/application/mock-get-all-places.ts
@@ -3,8 +3,8 @@ import { MockPlaceRepo } from '../../../modules/places/repos/mock-place-repo'
 import { GetAllPlacesController } from '../../../modules/places/use-cases/get-all-places/get-all-places-controller'
 import { GetAllPlacesUseCase } from '../../../modules/places/use-cases/get-all-places/get-all-places-use-case'
 
-const mockGetAllPlaces = (place: Array<Place> = []) => {
-  const placeRepo = new MockPlaceRepo(place)
+const mockGetAllPlaces = (places: Array<Place> = []) => {
+  const placeRepo = new MockPlaceRepo(places)
   const getAllPlacesUseCase = new GetAllPlacesUseCase(placeRepo)
   const getAllPlacesController = new GetAllPlacesController(getAllPlacesUseCase)
 
